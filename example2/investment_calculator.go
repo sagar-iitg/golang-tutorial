ef9d12c2d668a50537198be3720514a947d5e5c4
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-03-21 16:09:21 +0530

    Update investment_calculator.go

--- example2/investment_calculator.go
+++ example2/investment_calculator.go
@@ -10,7 +10,10 @@ func main() {
 	var investmentAmount = 1000
 	var expectedReturn = 5.5
 	var years = 10
+	fmt.Print("------\n")
+	fmt.Scan(&investmentAmount)
 	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturn/100, float64(years))
+
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, float64(years))
 	fmt.Println(futureValue)
 	fmt.Println(futureRealValue)
