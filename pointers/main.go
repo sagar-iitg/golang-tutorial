44ab7d09048924911ecf1563e58c8ea7ff47b5c5
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-03-22 01:58:03 +0530

    Update main.go

--- pointers/main.go
+++ pointers/main.go
@@ -1,10 +1,24 @@
 package main
 
+import "fmt"
+
+func pointer() {
+	var x *int
+	fmt.Println("Value of pointer is: ", x)
+
+	myNumber := 23
+	var ptr = &myNumber
+	fmt.Println("Value of actual data is: ", *ptr)
+	fmt.Println("Value of pointer is: ", ptr)
+	*ptr = *ptr + 5
+	fmt.Print(myNumber)
+
+}
 func main() {
 	var x int = 10
-	var p *int
-	p = &x
+	p := &x
 	println(x)
 	println(*p)
+	pointer()
 
 }
