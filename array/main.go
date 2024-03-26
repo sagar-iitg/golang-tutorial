11ec5c32de18591e9bef07b67096956bfe9954ee
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-03-26 15:41:17 +0530

    Update main.go

--- array/main.go
+++ array/main.go
@@ -12,5 +12,14 @@ func main() {
 	fruitList[1] = "Tomato"
 	fruitList[3] = "Banana"
 	fmt.Print("fruitList is", fruitList)
+	array()
+
+}
+func array() {
+
+	//compiler count the array elements for us
+	b := [...]string{"Penn", "Teller"}
+	fmt.Println()
+	fmt.Println(b)
 
 }
