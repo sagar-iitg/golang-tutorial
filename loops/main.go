c0d5f997f01c16c627d728a924e2490f66dd0f69
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-03-27 16:54:00 +0530

    Create main.go

---
+++ loops/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	days := []string{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}
+	fmt.Println(days)
+
+	for d := 0; d < len(days); d++ {
+		fmt.Println(days[d])
+	}
+
+	for i := range days {
+		fmt.Println(days[i])
+	}
+
+	for index, day := range days {
+		fmt.Println(index, day)
+	}
+
+}
