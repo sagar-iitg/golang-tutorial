443e73b7496de2c133f34a3e80d902c8cedea3cb
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-04-16 13:07:08 +0530

    Create main.go

---
+++ slice/exmaple1/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+//function to modify slice
+func modifySlice(numbers []int) {
+	//modify the slice
+
+	for i := range numbers {
+		numbers[i] *= 2
+	}
+
+}
+
+func main() {
+
+	//create slice
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fmt.Println("Original Slice: ", numbers)
+	modifySlice(numbers)
+
+}
