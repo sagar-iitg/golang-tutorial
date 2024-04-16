9e339e358b7dd82db01636f34b8402f727149110
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-04-16 15:47:36 +0530

    Update main.go

--- slice/exmaple1/main.go
+++ slice/exmaple1/main.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 //function to modify slice
-func modifySlice(numbers []int) {
+func modifySlice(numbers *[]int) {
 	//modify the slice
 
-	for i := range numbers {
-		numbers[i] *= 2
+	for i := range *numbers {
+		(*numbers)[i] *= 2
 	}
 
 }
@@ -17,6 +17,7 @@ func main() {
 	//create slice
 	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	fmt.Println("Original Slice: ", numbers)
-	modifySlice(numbers)
+	modifySlice(&numbers)
+	fmt.Println("Modified Slice: ", numbers)
 
 }
